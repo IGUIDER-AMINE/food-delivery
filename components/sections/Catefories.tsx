991a791593970ccde0cb688d1c25e46b4d36a8dc
IGUIDER-AMINE <amineiguider2017@gmail.com>
2023-08-08 16:01:18 +0200

    push layout

---
+++ components/sections/Catefories.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Catefories = () => {
+  return <div>Catefories</div>;
+};
+
+export default Catefories;
---
+++ components/sections/Chief.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Chief = () => {
+  return <div>Chief</div>;
+};
+
+export default Chief;
