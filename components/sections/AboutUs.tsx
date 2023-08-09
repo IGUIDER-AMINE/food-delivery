cf7fe51f8cb462b357869d7f2b5927b44bb1c881
IGUIDER-AMINE <amineiguider2017@gmail.com>
2023-08-09 11:14:28 +0200

    almost finish the project

--- components/sections/AboutUs.tsx
+++ components/sections/AboutUs.tsx
@@ -3,12 +3,12 @@ import React from "react";
 const AboutUs = () => {
   return (
     <section id="Banner" className="container mx-auto px-4">
-      <div className="container mx-auto flex flex-col lg:flex-row p-4 space-y-2 lg:space-x-4 rounded-3xl bg-standardGreen text-white">
-        <h1 className="font-titleFont text-2xl">
+      <div className="container mx-auto flex flex-col lg:flex-row p-3 lg:space-x-4 xl:px-12 gap-4 rounded-3xl bg-standardGreen text-white">
+        <h1 className="font-titleFont py-4 text-2xl self-center">
           What is the speciality of Vietnam food
         </h1>
         <div className="w-full h-[0.2px] lg:h-auto lg:w-[0.2px] bg-standarGray"></div>
-        <p className="font-normal text-standarGray">
+        <p className="font-normal py-4 self-center text-standarGray">
           The specialty of Vietnamese food lies in its unique blend of flavors,
           textures, and cultural influences that create a truly captivating
           culinary experience. Several key elements contribute to the
